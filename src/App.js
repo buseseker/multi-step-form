700f68de52a40cbfc4228b868d39bcf6e13883c2
buseseker <sekerbuse_96@hotmail.com>
2024-02-08 23:41:01 +0300

    Route for summary page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import PersonelInfo from './components/personel-info/PersonelInfo';
 import SelectPlan from './components/select-plan/SelectPlan';
 import AddOns from './components/add-ons/AddOns';
+import Summary from './components/summary/Summary';
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +11,7 @@ function App() {
         <Route path='personel-info' element={<PersonelInfo />} />
         <Route path='select-plan' element={<SelectPlan />} />
         <Route path='add-ons' element={<AddOns />} />
+        <Route path='summary' element={<Summary />} />
         <Route path='/' element={<Navigate to='/personel-info' />} />
       </Routes>
     </BrowserRouter>
