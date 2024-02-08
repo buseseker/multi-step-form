ef50a034fcf72fd85cacfcffbd05a6766a587535
buseseker <sekerbuse_96@hotmail.com>
2024-02-08 20:43:05 +0300

    Route for add-ons page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
 import PersonelInfo from './components/personel-info/PersonelInfo';
 import SelectPlan from './components/select-plan/SelectPlan';
+import AddOns from './components/add-ons/AddOns';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='personel-info' element={<PersonelInfo />} />
         <Route path='select-plan' element={<SelectPlan />} />
+        <Route path='add-ons' element={<AddOns />} />
         <Route path='/' element={<Navigate to='/personel-info' />} />
       </Routes>
     </BrowserRouter>
