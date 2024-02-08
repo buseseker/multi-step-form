afaa5ced6fa80f68ade774f68fa9d73cc0e55e6e
buseseker <sekerbuse_96@hotmail.com>
2024-02-08 16:58:45 +0300

    assets folder moved into src folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      fontSize:{
-        'h5': '0.83em',
-        'h1': '2em',
+      fontSize: {
+        h5: '0.83em',
+        h1: '2em',
       },
       fontFamily: {
-        'ubuntu': ['Ubuntu', 'sans-serif'],
+        ubuntu: ['Ubuntu', 'sans-serif'],
       },
       colors: {
         'light-gray': 'hsl(229, 24%, 87%)',
         'cool-gray': 'hsl(231, 11%, 63%)',
         'marine-blue': 'hsl(213, 96%, 18%)',
-        'button-hover-blue': 'hsl(213, 96%, 30%)'
+        'button-hover-blue': 'hsl(213, 96%, 30%)',
+        'magnolia': 'hsl(217, 100%, 97%)',
+        'alabaster': 'hsl(231, 100%, 99%)'
       },
       backgroundImage: {
-        'desktop-sidebar': "url('/assets/images/bg-sidebar-desktop.svg')"
-      }
+        'desktop-sidebar': "url('file:///C:/Users/seker/Desktop/multi-step-form/multi-step-form/src/assets/images/bg-sidebar-desktop.svg')",
+      },
     },
   },
   plugins: [],
-}
+};
