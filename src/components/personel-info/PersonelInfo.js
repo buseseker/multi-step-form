50cf4f01e3de3185cbcabf3f5dd7681c11b3a665
buseseker <sekerbuse_96@hotmail.com>
2024-02-08 23:44:34 +0300

    Sidebar step number design

--- src/components/personel-info/PersonelInfo.js
+++ src/components/personel-info/PersonelInfo.js
@@ -32,8 +32,8 @@ const PersonelInfo = () => {
       <div className='form-container bg-white flex h-[65vh] w-1/2 p-4 rounded-lg'>
         <div className='form-sidebar h-full w-[45%] bg-desktop-sidebar bg-center bg-no-repeat bg-cover rounded-xl flex flex-col p-8 gap-y-8'>
           <div className='step flex items-center gap-x-4 '>
-            <div className='step-number flex justify-center items-center border border-white w-10 h-10 rounded-full'>
-              <p className='text-white'>1</p>
+            <div className='step-number flex justify-center items-center bg-light-blue w-10 h-10 rounded-full'>
+              <p className='text-black font-extrabold'>1</p>
             </div>
             <div className='step-text'>
               <h5 className='text-cool-gray font-thin text-h5'>STEP 1</h5>
