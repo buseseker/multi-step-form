94c680986335920c73eee47b27d8da3e169c0fd5
buseseker <sekerbuse_96@hotmail.com>
2024-02-26 16:23:05 +0300

    routing arranged

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import PersonelInfo from './components/personel-info/PersonelInfo';
 import SelectPlan from './components/select-plan/SelectPlan';
 import AddOns from './components/add-ons/AddOns';
 import Summary from './components/summary/Summary';
+import SelectedServiceProvider from './context/SelectedServiceContext'; // Context'u doğru şekilde içe aktardık
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='personel-info' element={<PersonelInfo />} />
-        <Route path='select-plan' element={<SelectPlan />} />
-        <Route path='add-ons' element={<AddOns />} />
-        <Route path='summary' element={<Summary />} />
-        <Route path='/' element={<Navigate to='/personel-info' />} />
-      </Routes>
+      <SelectedServiceProvider> {/* SelectedServiceProvider bileşenini burada kullanmalısınız */}
+        <Routes>
+          <Route path='/personel-info' element={<PersonelInfo />} />
+          <Route path='/select-plan' element={<SelectPlan />} />
+          <Route path='/add-ons' element={<AddOns />} />
+          <Route path='/summary' element={<Summary />} />
+          <Route path='/' element={<Navigate to='/personel-info' />} />
+        </Routes>
+      </SelectedServiceProvider> {/* Kapatma etiketini ekledik */}
     </BrowserRouter>
   );
 }
