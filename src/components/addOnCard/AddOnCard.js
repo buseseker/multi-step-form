f8a388b67852389366aee2f65cefbb8033f08957
buseseker <sekerbuse_96@hotmail.com>
2024-02-26 18:03:15 +0300

    addOnCard component created

---
+++ src/components/addOnCard/AddOnCard.js
@@ -0,0 +1,47 @@
+import React, { useContext } from 'react';
+import { SelectedServiceContext } from '../../context/SelectedServiceContext';
+
+const AddOnCard = ({ addOn }) => {
+
+    const { selectedAddOns, setSelectedAddOns, selectedPlan, selectedAddOnPrices, setSelectedAddOnPrices } = useContext(SelectedServiceContext);
+
+    const handleCheckBox = (id) => {
+        const price = addOn[id]; // Eklenti ID'sine göre fiyatı al
+        if (selectedAddOns.includes(id)) {
+          setSelectedAddOns(selectedAddOns.filter((addOnId) => addOnId !== id));
+          const { [id]: removedKey, ...rest } = selectedAddOnPrices; // Seçilen ek özelliğin fiyatını kaldır
+          setSelectedAddOnPrices(rest); // Kalan fiyatları güncelle
+        } else {
+          setSelectedAddOns([...selectedAddOns, id]);
+          setSelectedAddOnPrices({ ...selectedAddOnPrices, [id]: price }); // Eklenti fiyatlarını sakla
+        }
+      };
+
+      const isSelected = (card) => selectedAddOns.includes(card);
+
+      console.log(selectedAddOns);
+      console.log(selectedAddOnPrices);
+  return (
+    <div className={`add-on flex justify-between items-center w-full border ${isSelected(addOn.id) ? 'border-violet-900 bg-indigo-50' : 'border-light-gray'} rounded-lg h-20 px-5`}>
+      <div className='flex gap-x-5'>
+        <label className='inline-flex items-center cursor-pointer'>
+          <input
+            type='checkbox'
+            className='form-checkbox border-gray-300 rounded-md w-5 h-5 accent-indigo-600 checked:border-transparent focus:ring-blue-400'
+            checked={isSelected(addOn.id)}
+            onChange={() => handleCheckBox(addOn.id)}
+          />
+        </label>
+        <div className='add-on-text'>
+          <h5 className='text-marine-blue font-extrabold'>{addOn.name}</h5>
+          <p className='text-cool-gray'>{addOn.description}</p>
+        </div>
+      </div>
+      <div className='price'>
+        <p>{selectedPlan === 'monthly' ? `+$${addOn.monthly}/mo` :  `+$${addOn.yearly}/yr`}</p>
+      </div>
+    </div>
+  );
+};
+
+export default AddOnCard;
