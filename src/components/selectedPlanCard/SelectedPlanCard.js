077898bf14bc4d863a7944a9ff25151279718d94
buseseker <sekerbuse_96@hotmail.com>
2024-02-28 15:52:13 +0300

    Component created

---
+++ src/components/selectedPlanCard/SelectedPlanCard.js
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import { SelectedServiceContext } from '../../context/SelectedServiceContext';
+
+const SelectedPlanCard = ({ plan }) => {
+  const { selectedPlan, setSelectedPlan, selectedCard, setSelectedCard } = useContext(SelectedServiceContext);
+
+  const handleCardClick = (plan) => {
+    if (selectedCard === plan.name) {
+      setSelectedCard(null);
+    } else {
+      setSelectedCard(plan.name);
+      setSelectedPlan({ id: plan.id, type: selectedPlan.type });
+    }
+  };
+
+  return (
+    <div
+      className={`card w-1/3 rounded-lg p-5 cursor-pointer ${selectedCard === plan.name ? 'border border-violet-900' : 'border border-light-gray'}`}
+      onClick={() => handleCardClick(plan)}
+    >
+      <img className='mb-10' src={plan.icon} alt='' />
+      <h5 className='text-marine-blue font-extrabold'>{plan.name}</h5>
+      <h5 className='text-cool-gray'>{selectedPlan.type === 'yearly' ? `$${plan.yearly}/yr` : `$${plan.monthly}/mo`}</h5>
+      {selectedPlan === 'yearly' && <p className='text-xs text-marine-blue'>2 months free</p>}
+    </div>
+  );
+};
+
+export default SelectedPlanCard;
