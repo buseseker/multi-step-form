3d9e0b594860ac8402a5cd4998f27e5ad094ca7d
buseseker <sekerbuse_96@hotmail.com>
2024-02-28 15:52:29 +0300

    plans and addOns object created

--- src/context/SelectedServiceContext.js
+++ src/context/SelectedServiceContext.js
@@ -1,16 +1,29 @@
 import React, { createContext, useState } from 'react';
+import iconAdvanced from '../assets/images/icon-advanced.svg';
+import iconArcade from '../assets/images/icon-arcade.svg';
+import iconPro from '../assets/images/icon-pro.svg';
 
 export const SelectedServiceContext = createContext();
 
 const SelectedServiceProvider = ({ children }) => {
   const [selectedCard, setSelectedCard] = useState(null);
-  const [selectedAddOns, setSelectedAddOns] = useState([]);
-  const [selectedPlan, setSelectedPlan] = useState('yearly');
-  const [selectedPrice, setSelectedPrice] = useState({});
-  const [selectedAddOnPrices, setSelectedAddOnPrices] = useState({});
+  const [selectedAddOnIds, setSelectedAddOnIds] = useState([]);
+  const [selectedPlan, setSelectedPlan] = useState({ id: '', type: 'yearly' });
+
+  const plans = [
+    { id: '1', name: 'Arcade', yearly: 90, monthly: 9, icon: iconArcade },
+    { id: '2', name: 'Advanced', yearly: 120, monthly: 12, icon: iconAdvanced },
+    { id: '3', name: 'Pro', yearly: 150, monthly: 15, icon: iconPro },
+  ];
+
+  const addOns = [
+    { id: '1', name: 'Online Service', description: 'Access to multiplayer games', yearly: 10, monthly: 1 }, // Eklenti ID'sine göre fiyatları tutar
+    { id: '2', name: 'Larger Storage', description: 'Extra 1TB of cloud save', yearly: 20, monthly: 2 },
+    { id: '3', name: 'Customizable Profile', description: 'Custom theme on your profile', yearly: 20, monthly: 2 },
+  ];
 
   return (
-    <SelectedServiceContext.Provider value={{ selectedPlan, setSelectedPlan, selectedCard, setSelectedCard, selectedAddOns, setSelectedAddOns, selectedPrice, setSelectedPrice, selectedAddOnPrices, setSelectedAddOnPrices }}>
+    <SelectedServiceContext.Provider value={{ selectedPlan, setSelectedPlan, selectedCard, setSelectedCard, selectedAddOnIds, setSelectedAddOnIds, addOns, plans }}>
       {children}
     </SelectedServiceContext.Provider>
   );
