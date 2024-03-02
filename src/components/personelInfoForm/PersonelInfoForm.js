59ba39119e65e61656384219a9c400ba498fb0ee
buseseker <sekerbuse_96@hotmail.com>
2024-03-02 05:23:53 +0300

    Responsive CSS

--- src/components/personelInfoForm/PersonelInfoForm.js
+++ src/components/personelInfoForm/PersonelInfoForm.js
@@ -29,12 +29,12 @@ const PersonelInfoForm = () => {
   };
 
   return (
-    <div className='form-section flex items-center flex-col gap-y-8 px-24 pt-10 relative  xs:px-6 xs:pb-10 sm:pb-10 lg:pb-0 lg:w-full xs:w-[95%] sm:w-[95%] xs:mx-auto sm:mx-auto xs:-my-16 sm:-my-16 lg:my-0 xs:bg-white sm:bg-white xs:rounded-lg sm:rounded-lg'>
-      <div className='form-header w-full flex flex-col gap-y-1'>
+    <div className='form-section flex items-center flex-col gap-y-8 pt-10 relative lg:px-24  xs:px-6 xs:pb-10 sm:pb-10 lg:pb-0 lg:w-full xs:w-[95%] sm:w-[95%] xs:mx-auto sm:mx-auto xs:-my-16 sm:-my-16 lg:my-0 xs:bg-white sm:bg-white xs:rounded-lg sm:rounded-lg'>
+      <div className='form-header w-full flex flex-col gap-y-1 lg:px-16 sm:px-24'>
         <h1 className='text-h1 text-marine-blue font-black xs:text-m'>Personal info</h1>
         <p className='text-cool-gray '>Please provide your name, email address, and phone number.</p>
       </div>
-      <div className='w-full'>
+      <div className='w-full lg:px-16 sm:px-24 '>
         <form className='flex flex-col gap-y-5' action=''>
           <div className='name-input'>
             <div className='label-err flex justify-between'>
